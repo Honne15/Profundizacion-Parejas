65d67459e19ce6957859c191b3ae68a485544b5a
Maria <julianahenao1510@gmail.com>
2025-02-18 17:58:36 -0500

    Agregamos prueba unitaria

---
+++ PruebaTDD/PruebaTDD.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace PruebaTDD.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(15, "SunMoon")]
+        [InlineData(9, "Sun")]
+        [InlineData(10, "Moon")]
+        [InlineData(7, "No es múltiplo de 3 ni de 5")]
+        public void GetNumber(int number, string word)
+        {
+            var result = Program.NumberVerify(number);
+            Assert.Equal(result, word);
+        }
+    }
+}
